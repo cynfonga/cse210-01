b55a1e5815be9c7f8d52a310538ffd5c242fa5dd
Cyrille <77592384+cynfonga@users.noreply.github.com>
2022-12-07 00:45:50 +0200

    Add files via upload

---
+++ jumper.py
@@ -0,0 +1,37 @@
+# TODO: Implement the Seeker class as follows...
+
+# 1) Add the class declaration. Use the following class comment.
+
+    """The person looking for the Hider. 
+    
+    The responsibility of a Seeker is to keep track of its location and distance travelled.
+    
+    Attributes:
+        location (int): The location of the Seeker (1-1000).
+
+    """
+    class Jumper:
+
+# 2) Create the class constructor. Use the following method comment.
+        """Constructs a new Seeker.
+
+        Args:
+            self (Seeker): An instance of Seeker.
+        """
+        def _init_( self):
+            self._jumper_list[]
+       
+# 3) Create the get_location(self) method. Use the following method comment.
+        """Gets the current location.
+        
+        Returns:
+            number: The current location,
+        """
+
+        def _get_location():
+            return
+
+        
+# 4) Create the move_location(self, location) method. Use the following method comment.
+     def move_location(self, location):
+        
